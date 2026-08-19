47ebaf212b62165f9b7157569d5add30018772f3
Priyanshu Ben <priyanshuben42@gmail.com>
2026-08-19 20:56:48 +0530

    added CRUD routes

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const Listing = require(`./models/listing.js`)
 const mongoose = require("mongoose")
 const engine = require('ejs-mate')
 const MONGO_URL = `mongodb://127.0.0.1:27017/major`;
-
+const methodOverride = require("method-override");
 
 // Middlewares
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(methodOverride("_method"));
 
 app.engine('ejs', engine);
 app.set("view engine", "ejs");
@@ -37,6 +37,7 @@ app.get("/", (req, res) => {
     res.send("Server Running!")
 })
 
+
 // Listings Route
 app.get("/listings", async (req, res) => {
     const showAll = await Listing.find({});
@@ -44,6 +45,25 @@ app.get("/listings", async (req, res) => {
 
 })
 
+// Search Route
+app.get("/listings/search", async (req, res) => {
+    const { q } = req.query;
+    if (!q || q.trim() === "") {
+        return res.redirect("/listings");
+    }
+    console.log(q);
+    const listings = await Listing.find({
+        title: {
+            $regex: q,
+            $options: "i"
+        }
+    });
+
+    res.render("listings/index", {
+        showAll: listings
+    });
+
+})
 
 // Create Form Route
 app.get("/listings/create", async (req, res) => {
@@ -54,6 +74,36 @@ app.get("/listings/create", async (req, res) => {
 app.post("/listings/create", async (req, res) => {
     const listing = await Listing.create(req.body.listing);
     res.redirect('/listings')
+})
+
+// Show (Read) Route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { e: listing })
+})
+
+
+// Edit (Render Form)
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render('listings/edit.ejs', { e: listing })
+})
+
+// Patch Request (Update Route)
+app.put("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    console.log(req.body.listing)
+    const listing = await Listing.findByIdAndUpdate(id, req.body.listing);
+    res.redirect(`/listings/${id}`)
+})
+
+// Delete Request (Destroy Route)
+app.delete("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findByIdAndDelete(id);
+    res.redirect(`/listings`)
 })
 
 // Listen
